bed5f122a2adf86aa30bbb14db21f7069e1a272f
la3jiz <lazizamin8@gmail.com>
2024-02-15 23:02:53 +0100

    getCurrentConsultant controller function

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,136 +1,159 @@
 const userModel = require("../models/userModel");
 const Preregister = require("../models/preRegistrationModel");
-const validateLoginInput = require('../validations/login')
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const validateRegisterInput = require('../validations/validateRegisterInput');
+const validateLoginInput = require("../validations/login");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validateRegisterInput = require("../validations/validateRegisterInput");
 const preRegistrationModel = require("../models/preRegistrationModel");
 const authUser = async (req, res) => {
-    try {
-      const { errors, isValid } = validateLoginInput(req.body);
+  try {
+    const { errors, isValid } = validateLoginInput(req.body);
 
-      if (!isValid) {
-        return res.status(404).json(errors);
-      }
+    if (!isValid) {
+      return res.status(404).json(errors);
+    }
 
-      const user = await userModel.findOne({ email: req.body.email });
+    const user = await userModel.findOne({ email: req.body.email });
 
-      if (user && user.googleId) {
-        errors.email = "Cannot login with email and password. Please use Google Sign In.";
-        return res.status(400).json(errors);
-      }
+    if (user && user.googleId) {
+      errors.email =
+        "Cannot login with email and password. Please use Google Sign In.";
+      return res.status(400).json(errors);
+    }
 
-      if (!user) {
-        errors.email = "Email not found";
-        return res.status(404).json(errors);
-      }
+    if (!user) {
+      errors.email = "Email not found";
+      return res.status(404).json(errors);
+    }
 
-      const isMatch = await bcrypt.compare(req.body.password, user.password);
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    const preRegistration = await Preregister.findOne({
+      userId: user._id,
+    }).populate("contractProcess");
+    console.log(preRegistration);
+    if (isMatch) {
+      const token = jwt.sign(
+        {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+          verified: user.verified,
+          profile: user.profile,
+          isBlocked: user.isBlocked,
+          onligne: user.onligne,
+          firstLogin: user.firstLogin,
+          driverIsVerified: user.driverIsVerified,
+          preRegister: preRegistration,
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: Number.MAX_SAFE_INTEGER }
+      );
+      console.log(token);
 
-      const preRegistration = await Preregister.findOne({ userId: user._id }).populate('contractProcess');
-console.log(preRegistration)
-      if (isMatch) {
-        const token = jwt.sign(
-          {
-            id: user._id,
-            name: user.name,
-            email: user.email,
-            role: user.role,
-            verified: user.verified,
-            profile: user.profile,
-            isBlocked: user.isBlocked,
-            onligne: user.onligne,
-            firstLogin: user.firstLogin,
-            driverIsVerified: user.driverIsVerified,
-            preRegister: preRegistration,
-          },
-          process.env.SECRET_KEY,
-          { expiresIn: Number.MAX_SAFE_INTEGER }
-        );
-        console.log(token)
-
-        return res.header("auth-token", token).status(200).json({
+      return res
+        .header("auth-token", token)
+        .status(200)
+        .json({
           status: true,
           token: "Bearer " + token,
           role: user.role,
           id: user._id,
           preRegistration: preRegistration,
-          status: preRegistration?.status
+          status: preRegistration?.status,
         });
-      } else {
-        errors.password = "Password incorrect";
-        return res.status(404).json(errors);
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ success: false, message: "Internal Server Error" });
+    } else {
+      errors.password = "Password incorrect";
+      return res.status(404).json(errors);
     }
-  };
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
 
 const registerUser = async (req, res) => {
-    try {
-        const { errors, isValid } = validateRegisterInput(req.body);
+  try {
+    const { errors, isValid } = validateRegisterInput(req.body);
 
-        if (!isValid) {
-            return res.status(400).json(errors);
-        }
-
-        const user = await userModel.findOne({ email: req.body.email });
-
-        if (user) {
-            errors.email = "Email already exists";
-            return res.status(400).json(errors);
-        }
-
-        const newUser = new userModel({
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, 10),
-
-        });
-        const preRegistration = new preRegistrationModel({userId : newUser._id});
-        await preRegistration.save();
-
-        newUser.preRegister = preRegistration._id;
-
-        const savedUser = await newUser.save();
-        return res.status(200).json(savedUser);
-
-
+    if (!isValid) {
+      return res.status(400).json(errors);
     }
-    catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal Server Error" });
+
+    const user = await userModel.findOne({ email: req.body.email });
+
+    if (user) {
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     }
-}
+
+    const newUser = new userModel({
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10),
+    });
+    const preRegistration = new preRegistrationModel({ userId: newUser._id });
+    await preRegistration.save();
+
+    newUser.preRegister = preRegistration._id;
+
+    const savedUser = await newUser.save();
+    return res.status(200).json(savedUser);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 const getAllConsultant = async (req, res) => {
-    try {
-        const consultants = await userModel.find({ role: "CONSULTANT" }).populate("preRegister");
-        return res.status(200).json(consultants);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal Server Error" });
-    }
-}
+  try {
+    const consultants = await userModel
+      .find({ role: "CONSULTANT" })
+      .populate("preRegister");
+    return res.status(200).json(consultants);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 const getConsultantById = async (req, res) => {
-    try {
-        const consultant = await userModel.find({preRegister:req.params.id}).populate("preRegister");
-        return res.status(200).json(consultant);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal Server Error" });
-    }
-}
+  try {
+    const consultant = await userModel
+      .find({ preRegister: req.params.id })
+      .populate("preRegister");
+    return res.status(200).json(consultant);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
-
-
-
-
+const getCurrentConsultantById = async (req, res) => {
+  try {
+    const consultant = await userModel
+      .findById(req.user.id)
+      .populate("preRegister");
+    return res.status(200).json({
+      data: consultant,
+      action: "userController.js/getCurrentConsultantById",
+      status: "success",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      status: error,
+      action: "userController.js/getCurrentConsultantById",
+      message: "Internal Server Error",
+    });
+  }
+};
 
 module.exports = {
-        authUser,
-        registerUser,
-        getAllConsultant,
-        getConsultantById
-    }
+  authUser,
+  registerUser,
+  getAllConsultant,
+  getConsultantById,
+  getCurrentConsultantById,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const protect = require('../middleware/authMiddleware.js');
 const express = require('express');
 const { ROLES, isRole, isResetTokenValid } = require('../security/Rolemiddleware');
-const { authUser, registerUser, getAllConsultant, getConsultantById } = require('../controllers/users.controller.js');
+const { authUser, registerUser, getAllConsultant, getConsultantById, getCurrentConsultantById } = require('../controllers/users.controller.js');
 const { createPreRegistration1, createPreRegistration2, getPreregistration, getPendingPreregistration, getConsultantStats, validatePreregistrationClientInfo, createPreRegistration4, createPreRegistration3 } = require('../controllers/preregistration.Controller.js');
 const router = express.Router()
 const storage = multer.diskStorage({});
@@ -29,4 +29,5 @@ router.route('/preregistartion/getPendingPreregistration').get(passport.authenti
 router.route('/getConsultantStats').get(passport.authenticate('jwt', {session: false}),isRole(ROLES.CONSULTANT,ROLES.RH),getConsultantStats)
 router.route('/getConsultantById/:id').get(passport.authenticate('jwt', {session: false}),isRole(ROLES.CONSULTANT, ROLES.RH),getConsultantById)
 router.route('/validatePreregistrationClientInfo/:id').put(passport.authenticate('jwt', {session: false}),isRole( ROLES.CONSULTANT,ROLES.RH),validatePreregistrationClientInfo)
+router.route('/getCurrantConsultant').get(passport.authenticate('jwt', {session: false}),isRole(ROLES.CONSULTANT),getCurrentConsultantById)
 module.exports = router
